0d514d2b5af77edf695c6223567c04063e654e99
SharaMulti <jjuulliiooooooooo@gmail.com>
2025-02-24 09:15:42 -0600

    My personal info

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+/*
+Amerike university
+Author: Julio López
+Project n:2
+Date: 2/04/25
+Description: Present my personal data on screen*/
+
+#include <iostream>
+ 
+using namespace std;
+
+
+int main () {
+
+    cout<< "Julio César López Ponce\n";
+    cout<< "18\n";
+    cout<< "Software\n";
+    cout<< "CDMX\n";
+    cout<< "Play videogames\n";
+    cout<< "Anything with pasta\n";
+    cout<< "Monster by Skillet\n";
+
+    return 0;
+}
